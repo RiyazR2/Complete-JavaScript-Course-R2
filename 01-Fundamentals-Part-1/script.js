4bb8f1bd383a49e7c76cd611620f544e3d21d990
RiyazR2 <riyazpathan193.rp@gmail.com>
2023-09-09 22:30:52 +0530

    Assignment 1 completed

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -2,15 +2,33 @@
 // let js = "Amazing";
 // if (js === "Amazing") alert("Hello World ! \nJavaScript is FUN!");
 
-// V2 :-Values and Variables
-console.log(40 + 8 + 9 - 7 * 2 + 7);
-console.log("String :- JavaScript");
-console.log(11);
+// // V2 :-Values and Variables
+// console.log(40 + 8 + 9 - 7 * 2 + 7);
+// console.log("String :- JavaScript");
+// console.log(11);
 
-let firstName = "Riyaz"; //Variable Creation
-console.log(firstName);
+// let firstName = "Riyaz"; //Variable Creation
+// console.log(firstName);
 
-//only _underscore and $doller symbol is allowed with variable name and Digit not allowed at starting name of variable
-let $doller = 1;
-let _Rupee = 82.984;
-console.log($doller, "Doller = India Rupee", _Rupee);
+// //only _underscore and $doller symbol is allowed with variable name and Digit not allowed at starting name of variable
+// let $doller = 1;
+// let _Rupee = 82.984;
+// console.log($doller, "Doller = India Rupee", _Rupee);
+
+//Assignment 1
+//LECTURE: Values and Variables
+// 1. Declare variables called 'country', 'continent' and 'population' and
+// assign their values according to your own country (population in millions)
+// 2. Log their values to the console
+
+let country = "INDIA";
+let continent = "Asia";
+let population = 1431127090;
+console.log(
+  country,
+  "Country\n",
+  continent,
+  "Continent\n",
+  population,
+  "Population of India"
+);
