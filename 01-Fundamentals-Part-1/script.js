a1ac3f97c547167cf34d82c7204b47c8cadf0f82
RiyazR2 <riyazpathan193.rp@gmail.com>
2023-09-09 22:14:01 +0530

    values and Variables

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -1,2 +1,16 @@
-let js = "Amazing";
-if (js === "Amazing") alert("Hello World ! \nJavaScript is FUN!");
+// // V1
+// let js = "Amazing";
+// if (js === "Amazing") alert("Hello World ! \nJavaScript is FUN!");
+
+// V2 :-Values and Variables
+console.log(40 + 8 + 9 - 7 * 2 + 7);
+console.log("String :- JavaScript");
+console.log(11);
+
+let firstName = "Riyaz"; //Variable Creation
+console.log(firstName);
+
+//only _underscore and $doller symbol is allowed with variable name and Digit not allowed at starting name of variable
+let $doller = 1;
+let _Rupee = 82.984;
+console.log($doller, "Doller = India Rupee", _Rupee);
