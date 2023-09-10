4b4f2c50a133a1de44874de6082ef398beb8c5af
RiyazR2 <riyazpathan193.rp@gmail.com>
2023-09-10 11:44:09 +0530

    Data types

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -1,22 +1,27 @@
-// // V1
-// let js = "Amazing";
-// if (js === "Amazing") alert("Hello World ! \nJavaScript is FUN!");
+/* 
+// V1
+let js = "Amazing";
+if (js === "Amazing") alert("Hello World ! \nJavaScript is FUN!");
+*/
 
-// // V2 :-Values and Variables
-// console.log(40 + 8 + 9 - 7 * 2 + 7);
-// console.log("String :- JavaScript");
-// console.log(11);
+/*
+ // V2 :-Values and Variables
+console.log(40 + 8 + 9 - 7 * 2 + 7);
+console.log("String :- JavaScript");
+console.log(11);
 
-// let firstName = "Riyaz"; //Variable Creation
-// console.log(firstName);
+let firstName = "Riyaz"; //Variable Creation
+console.log(firstName);
 
-// //only _underscore and $doller symbol is allowed with variable name and Digit not allowed at starting name of variable
-// let $doller = 1;
-// let _Rupee = 82.984;
-// console.log($doller, "Doller = India Rupee", _Rupee);
+//only _underscore and $doller symbol is allowed with variable name and Digit not allowed at starting name of variable
+let $doller = 1;
+let _Rupee = 82.984;
+console.log($doller, "Doller = India Rupee", _Rupee);
+*/
 
+/*
 //Assignment 1
-//LECTURE: Values and Variables
+// LECTURE: Values and Variables
 // 1. Declare variables called 'country', 'continent' and 'population' and
 // assign their values according to your own country (population in millions)
 // 2. Log their values to the console
@@ -32,3 +37,26 @@ console.log(
   population,
   "Population of India"
 );
+*/
+
+// V3 Datatypes
+
+let JavaScriptIsFun = true;
+console.log(JavaScriptIsFun);
+
+console.log(typeof false);
+console.log(typeof JavaScriptIsFun);
+JavaScriptIsFun = "Riyaz"; //automatically change the type of JavaScriptIsFun Variable type to String
+console.log(typeof JavaScriptIsFun);
+console.log(typeof 33);
+console.log(typeof 10.23);
+console.log(typeof "Pathan");
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1997;
+console.log(year);
+
+console.log(typeof null); //type of null is Object
