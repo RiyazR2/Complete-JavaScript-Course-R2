6cc70e2b2cb69bbb299e819655ecbe18a42a24c6
RiyazR2 <riyazpathan193.rp@gmail.com>
2023-09-13 18:35:12 +0530

    Assignment 8 is Completed

--- 01-Fundamentals-Part-1/Assignments.js
+++ 01-Fundamentals-Part-1/Assignments.js
@@ -147,3 +147,37 @@ console.log("19" - "13" + "17"); // 617 or 191317 (right ans: 617)
 console.log("19" - "13" + 17); // 23
 console.log("123" < 57); //NaN (right ans: false)
 console.log(5 + 6 + "4" + 9 - 4 - 2); //11442 (right ans: 1143)
+
+/* Assignment 8 LECTURE: Equality Operators: == vs. ===
+1. Declare a variable 'numNeighbours' based on a prompt input like this:
+prompt('How many neighbour countries does your country
+have?');
+2. If there is only 1 neighbour, log to the console 'Only 1 border!' (use loose equality
+== for now)
+3. Use an else-if block to log 'More than 1 border' in case 'numNeighbours'
+is greater than 1
+4. Use an else block to log 'No borders' (this block will be executed when
+'numNeighbours' is 0 or any other value)
+5. Test the code with different values of 'numNeighbours', including 1 and 0.
+6. Change == to ===, and test the code again, with the same values of
+'numNeighbours'. Notice what happens when there is exactly 1 border! Why
+is this happening?
+7. Finally, convert 'numNeighbours' to a number, and watch what happens now
+when you input 1
+8. Reflect on why we should use the === operator and type conversion in this
+situation
+*/
+
+//Afghanistan, Pakistan, China, Bhutan, Nepal, Myanmar, Bangladesh, Maldives and Sri Lanka.(total 9)
+let numNeighbours = prompt(
+  "How many neighbour countries does your country have?"
+);
+if (numNeighbours == 1) {
+  console.log("Only 1 border!");
+} else if (numNeighbours >= 1) {
+  console.log("More than 1 border");
+} else {
+  console.log("No borders");
+}
+//when I use without Number conversion and ===(strict equality) its shows for data 1 More than 1 border then
+//After change I Use Number conversion and ===(strict equality) its shows for data 1 only 1 border
