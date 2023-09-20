02233441213e947221cd24d4c4d01970b58d55bf
RiyazR2 <riyazpathan193.rp@gmail.com>
2023-09-20 14:50:16 +0530

    Assignment 13 Completed

--- 02-Fundamental-Part-2/Assignments.js
+++ 02-Fundamental-Part-2/Assignments.js
@@ -234,3 +234,42 @@ console.log(populationArray2)
 console.log(percentageArray2)
 
 
+/*
+// Assignment 12 LECTURE: Looping Backwards and Loops in Loops
+1. Store this array of arrays into a variable called 'listOfNeighbours'
+[['Canada', 'Mexico'], ['Spain'], ['Norway', 'Sweden',
+'Russia']];
+2. Log only the neighbouring countries to the console, one by one, not the entire
+arrays. Log a string like 'Neighbour: Canada' for each country
+3. You will need a loop inside a loop for this. This is actually a bit tricky, so don't
+worry if it's too difficult for you! But you can still try to figure this out anyway �
+
+*/
+
+const listOfNeighbours = [['Canada', 'Mexico'], ['Spain'], ['Norway', 'Sweden', 'Russia']]
+
+for (let i = 0; i < listOfNeighbours.length; i++) {
+    //not understand question so the below code is copy of JONAS
+    for (let y = 0; y < listOfNeighbours[i].length; y++) {
+        console.log(`Neighbour: ${listOfNeighbours[i][y]}`);
+    }
+}
+
+
+/* Assignment 13 LECTURE: The while Loop
+1. Recreate the challenge from the lecture 'Looping Arrays, Breaking and Continuing',
+but this time using a while loop (call the array 'percentages3')
+2. Reflect on what solution you like better for this task: the for loop or the while
+loop?
+*/
+const percentages3 = [];
+let i = 0;
+while (i < populationArray.length) {
+    const perc = percentageOfWorld1(populationArray[i]).toFixed(2);
+    percentages3.push(perc);
+    i++;
+}
+console.log(percentages3);
+
+
+// ********************************************** Last 2 Assignment Not understand ***********************
