27bcd7fc25767f7c86b725347df67d40bd85efd2
RiyazR2 <riyazpathan193.rp@gmail.com>
2023-09-21 22:58:06 +0530

    Developer skills and setup section

--- 03-Developer-Skills/script.js
+++ 03-Developer-Skills/script.js
@@ -3,4 +3,8 @@
 const x = '23';
 if (x === 23) console.log(23);
 
-const calcAge = birthYear => 2023 - birthYear;
+const calcAge = (birthYear) => 2023 - birthYear;
+
+// Watching Developer Skills and Editor setup section
+
+// "arrowParens": "avoid"
