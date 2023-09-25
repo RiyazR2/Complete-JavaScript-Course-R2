93703fbd61d9087e1dd38f053183b91bf5090d6a
RiyazR2 <riyazpathan193.rp@gmail.com>
2023-09-25 12:26:04 +0530

    Logic for Number

--- 05-Guess-My-Number/script.js
+++ 05-Guess-My-Number/script.js
@@ -14,15 +14,22 @@ console.log(document.querySelector('.guess').value);
 let initialScore = 20;
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
 
-document.querySelector('.number').textContent = secretNumber;
-
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
+
+  // When their is no input
   if (!guess) {
     document.querySelector('.message').textContent = '⛔ No Number';
+
+    // When Player Wins
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = '🎉 Correct Number!';
+    document.querySelector('body').style.backgroundColor = '#60b347';
+
+    //Secret Number Display
+    document.querySelector('.number').textContent = secretNumber;
+    // When guess is too High
   } else if (guess > secretNumber) {
     if (initialScore > 1) {
       document.querySelector('.message').textContent = '📈 Too High!';
@@ -33,6 +40,8 @@ document.querySelector('.check').addEventListener('click', function () {
         'Game Over ! \n💥You Lost the GAME';
       document.querySelector('.score').textContent = 0;
     }
+
+    // When guess is too low
   } else if (guess < secretNumber) {
     if (initialScore > 1) {
       document.querySelector('.message').textContent = '📈 Too Low!';
