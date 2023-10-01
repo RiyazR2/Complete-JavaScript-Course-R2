641782b105c594cfe728457793763f5e2ca75bc9
RiyazR2 <riyazpathan193.rp@gmail.com>
2023-10-01 15:39:13 +0530

    Script Done with Comments

--- 07-Dice-Game/script.js
+++ 07-Dice-Game/script.js
@@ -1,12 +1,15 @@
 'use strict';
 
+// Selecting Player Element
 const player0El = document.querySelector('.player--0');
 const player1El = document.querySelector('.player--1');
-const diceEl = document.querySelector('.dice');
+
+// Selecting Buttons Element
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 const btnNew = document.querySelector('.btn--new');
 
+// Selecting Current(value) Element
 const current0El = document.getElementById('current--0');
 const current1El = document.getElementById('current--1');
 
@@ -14,8 +17,12 @@ const current1El = document.getElementById('current--1');
 const score0El = document.getElementById('score--0'); //43
 const score1El = document.getElementById('score--1'); //24
 
-// Setup initial value to score of players
+const diceEl = document.querySelector('.dice');
+
+// Declare Variables outside from block to use in different different Functions
 let scores, activePlayer, currentScore, playing;
+
+// Starting/Initial Conditions or Reseting Game Value
 const init = function () {
   scores = [0, 0];
   activePlayer = 0;
@@ -34,23 +41,27 @@ const init = function () {
   player1El.classList.remove('player--active');
 };
 
+// Invoking init() function for starting page i.e initial values
 init();
 
 // Selecting dice Element and hidding the Dice image at initial stage
 
+// Function for Switching player (Button Hold, dice=1)
 const switchPlayer = function () {
   document.getElementById(`current--${activePlayer}`).textContent = 0;
   currentScore = 0;
-  activePlayer = activePlayer === 0 ? 1 : 0; //(if AP is 0 then switch to 1 or if AP is not zero 0 i.e 1 then switch to 0)
+
+  //(if AP is 0 then switch to 1 or if AP is not zero 0 i.e 1 then switch to 0)
+  activePlayer = activePlayer === 0 ? 1 : 0;
 
   // toggle :- if class 'player--active' then it remove if it not active then it add it
   player0El.classList.toggle('player--active');
   player1El.classList.toggle('player--active');
 };
 
-diceEl.classList.add('hidden');
-
+// Rolling Dice Functionality
 btnRoll.addEventListener('click', function () {
+  //i.e when game started &  game is not finished
   if (playing) {
     // Generating Random Number for Dice
     const dice = Math.trunc(Math.random() * 6) + 1;
@@ -63,40 +74,46 @@ btnRoll.addEventListener('click', function () {
 
     // Check For Rolled 1
     if (dice !== 1) {
-      currentScore += dice;
+      currentScore += dice; // Add dice value to currentScore
       document.getElementById(`current--${activePlayer}`).textContent =
-        currentScore;
-
-      // current0El.textContent = currentScore;
+        currentScore; // i.e current0El or current1El.textContent = currentScore;
     } else {
-      // Switch Player
-
-      switchPlayer();
+      switchPlayer(); // Calling switchPlayer if dice === 1
     }
   }
 });
-//
 
 btnHold.addEventListener('click', function () {
+  // i.e when game started &  game is not finished
   if (playing) {
-    scores[activePlayer] += currentScore;
-    //   scores[1] = scores[1] + currentScore;
+    //Add current score to ActivePlayer Score
+    scores[activePlayer] += currentScore; //scores[1] = scores[1] + currentScore;
+
     document.getElementById(`score--${activePlayer}`).textContent =
       scores[activePlayer];
-    if (scores[activePlayer] >= 20) {
-      playing = false;
-      diceEl.classList.add('hidden');
 
+    //Check's is Player Score is Greater than or Equal to 20
+    if (scores[activePlayer] >= 20) {
+      // Game is Finish if this condition is TRUE
+
+      playing = false; //i.e buttons are not clickable
+
+      diceEl.classList.add('hidden'); // Dice Hidded
+
+      // Player Winner CSS style Class added
       document
         .querySelector(`.player--${activePlayer}`)
         .classList.add('player--winner');
+
+      //Removing Active Player becouse it game is won and finished
       document
         .querySelector(`.player--${activePlayer}`)
         .classList.remove('player--active');
     } else {
-      switchPlayer();
+      switchPlayer(); //Calling switchPlayer if dice === 1
     }
   }
 });
 
+//New Game Button i.e Restart
 btnNew.addEventListener('click', init);
