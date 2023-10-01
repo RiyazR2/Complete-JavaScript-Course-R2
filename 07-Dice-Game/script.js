91f07a5c6a2adc130371b3b26571beb62083bcce
RiyazR2 <riyazpathan193.rp@gmail.com>
2023-10-01 12:15:47 +0530

    Hold Score and Hide Dice

--- 07-Dice-Game/script.js
+++ 07-Dice-Game/script.js
@@ -57,6 +57,10 @@
 const player0El = document.querySelector('.player--0');
 const player1El = document.querySelector('.player--1');
 
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+const btnNew = document.querySelector('.btn--new');
+
 const current0El = document.getElementById('current--0');
 const current1El = document.getElementById('current--1');
 
@@ -77,41 +81,70 @@ let activePlayer = 0;
 //Current or Initial Score of Players
 let currentScore = 0;
 
+let playing = true;
+
 // Selecting dice Element and hidding the Dice image at initial stage
+
+const switchPlayer = function () {
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  currentScore = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0; //(if AP is 0 then switch to 1 or if AP is not zero 0 i.e 1 then switch to 0)
+
+  // toggle :- if class 'player--active' then it remove if it not active then it add it
+  player0El.classList.toggle('player--active');
+  player1El.classList.toggle('player--active');
+};
+
 const diceEl = document.querySelector('.dice');
 diceEl.classList.add('hidden');
 
-const btnRoll = document.querySelector('.btn--roll');
-
 btnRoll.addEventListener('click', function () {
-  // Generating Random Number for Dice
-  const dice = Math.trunc(Math.random() * 6) + 1;
-  console.log(dice);
+  if (playing) {
+    // Generating Random Number for Dice
+    const dice = Math.trunc(Math.random() * 6) + 1;
+    console.log(dice);
 
-  // Removing Hided Dice image i.e Displaying Dice
-  diceEl.classList.remove('hidden');
+    // Removing Hided Dice image i.e Displaying Dice
+    diceEl.classList.remove('hidden');
 
-  //Manipulating HTML img src Attribute
-  diceEl.src = `dice-${dice}.png`; // this is indirectly name of the Dice images
+    //Manipulating HTML img src Attribute
+    diceEl.src = `dice-${dice}.png`; // this is indirectly name of the Dice images
 
-  // Check For Rolled 1
-  if (dice !== 1) {
-    currentScore += dice;
-    score0El.textContent = dice;
-    document.getElementById(`current--${activePlayer}`).textContent =
-      currentScore;
+    // Check For Rolled 1
+    if (dice !== 1) {
+      currentScore += dice;
+      document.getElementById(`current--${activePlayer}`).textContent =
+        currentScore;
 
-    // current0El.textContent = currentScore;
-  } else {
-    // Switch Player
+      // current0El.textContent = currentScore;
+    } else {
+      // Switch Player
 
-    document.getElementById(`current--${activePlayer}`).textContent = 0;
-    currentScore = 0;
-    activePlayer = activePlayer === 0 ? 1 : 0; //(if AP is 0 then switch to 1 or if AP is not zero 0 i.e 1 then switch to 0)
-
-    // toggle :- if class 'player--active' then it remove if it not active then it add it
-    player0El.classList.toggle('player--active');
-    player1El.classList.toggle('player--active');
+      switchPlayer();
+    }
   }
 });
 //
+
+btnHold.addEventListener('click', function () {
+  if (playing) {
+    scores[activePlayer] += currentScore;
+    //   scores[1] = scores[1] + currentScore;
+    document.getElementById(`score--${activePlayer}`).textContent =
+      scores[activePlayer];
+    if (scores[activePlayer] >= 20) {
+      playing = false;
+      diceEl.classList.add('hidden');
+
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add('player--winner');
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.remove('player--active');
+      switchPlayer();
+    } else {
+      switchPlayer();
+    }
+  }
+});
