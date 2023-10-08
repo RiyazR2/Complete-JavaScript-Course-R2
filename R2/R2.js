a4d6bd3ad8e02c82ca04646bd1140e50560b440f
RiyazR2 <riyazpathan193.rp@gmail.com>
2023-10-08 13:14:44 +0530

    Added Javascript file for testing

---
+++ R2/R2.js
@@ -0,0 +1,10 @@
+function print() {
+  var square1 = number * number;
+  console.log(square1);
+  var number = 50;
+  var square2 = number * number;
+  console.log(square2);
+}
+print();
+// NaN
+// 2500
