b68d83c76ac95ab591f872b85c9f36387006e90d
RiyazR2 <riyazpathan193.rp@gmail.com>
2023-10-13 20:38:37 +0530

    Js File for Temp Use

--- R2/R2.js
+++ R2/R2.js
@@ -1,10 +1,3 @@
-function print() {
-  var square1 = number * number;
-  console.log(square1);
-  var number = 50;
-  var square2 = number * number;
-  console.log(square2);
-}
-print();
-// NaN
-// 2500
+"use strict";
+
+// Just used this file for testing
