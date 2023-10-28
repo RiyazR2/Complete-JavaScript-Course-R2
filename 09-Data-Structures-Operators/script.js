9749c0f8cb5316a214605a8f8b7b3e8f0838c428
RiyazR2 <riyazpathan193.rp@gmail.com>
2023-10-28 17:41:11 +0530

    Map Hald Done

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -478,3 +478,34 @@ rest.set(arr, 'Test');
 console.log(rest.get(arr)); // Test
 
 /******************************* Maps : Iterations *********************************/
+
+const question = new Map([
+  ['question', 'What is the best programming Language in the world?'],
+  [1, 'C'],
+  [2, 'Java'],
+  [3, 'Javascript'],
+  [true, 'Correct 🎉'],
+  [false, 'Try Again!'],
+]);
+console.log(question);
+
+// Convert Object to Map
+console.log(Object.entries(openingHours));
+const hoursMap = new Map(Object.entries(openingHours));
+console.log(hoursMap);
+
+//Quiz app
+// console.log(question.get('question'));
+// for (const [key, value] of question) {
+//   if (typeof key === 'number') console.log(`Answer ${key}: ${value}`);
+// }
+
+// const answer = Number(prompt('Your answer'));
+// console.log(answer);
+
+// console.log(question.get(question.get('correct') === answer));
+
+//covert map to array
+console.log([...question]);
+console.log([...question.keys()]);
+console.log([...question.values()]);
