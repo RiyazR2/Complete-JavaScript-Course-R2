31803ed3715523317e21452449d0b32d51e45148
RiyazR2 <riyazpathan193.rp@gmail.com>
2023-11-06 12:15:32 +0530

    CALL and APPLY

--- 10-Functions/script.js
+++ 10-Functions/script.js
@@ -1,6 +1,6 @@
 'use strict';
 /* ***************************** Default Parameters ***************************** */
-
+/*
 const bookings = [];
 
 const createBookings = function (
@@ -29,7 +29,7 @@ createBookings('LH123', 5);
 createBookings('LH123', undefined, 1000);
 
 console.log('**********************************************************');
-
+*/
 /* ***************************** How Passing Arguments Works: Value vs. Reference ***************************** */
 /*
 const flight = 'LH234';
@@ -58,10 +58,12 @@ const newPassport = function (person) {
 };
 newPassport(jonas);
 checkIn(flight, jonas);
-*/
+
 console.log('**********************************************************');
+*/
 
 /* ***************************** Functions Accepting Callback Functions ***************************** */
+/*
 
 //take a string remove space and convert to lowercase
 const oneWord = function (str) {
@@ -94,3 +96,60 @@ const high5 = function () {
 };
 document.body.addEventListener('click', high5);
 ['Jonas', 'Martha', 'Adam'].forEach(high5);
+
+*/
+
+/* ***************************** Functions Returning Functions ***************************** */
+
+const greet = function (greeting) {
+  return function (name) {
+    console.log(`${greeting} ${name}`);
+  };
+};
+
+const greeterHey = greet('Hey');
+greeterHey('Jonas');
+greeterHey('Riyaz');
+greet('Hello')('Riyaz');
+
+console.log('-----------------------------------------');
+
+// Using arrow function
+const greetArr = greeting => name => console.log(`${greeting} ${name}`);
+
+greet('Hi')('Riyaz');
+
+console.log('*****************************************');
+
+/* ***************************** The call and apply Methods ***************************** */
+
+const lufthansa = {
+  airline: 'Lufthansa',
+  iataCode: 'LH',
+  bookings: [],
+  //   book:function(){}
+  book(flightNum, name) {
+    console.log(
+      `${name} booked a seat on ${this.airline} flight ${this.iataCode}${flightNum}`
+    );
+    this.bookings.push({ fligh: `${this.iataCode}${flightNum}`, name });
+  },
+};
+
+lufthansa.book(239, 'Jonas Schmedtmann');
+lufthansa.book(635, 'John Smith');
+
+console.log('-----------------------------------------');
+const eurowings = {
+  name: 'Eurowings',
+  iataCode: 'EW',
+  bookings: [],
+};
+
+const book = lufthansa.book;
+
+// Does NOT work
+// book(23, 'Sarah Williams');
+
+book.call(eurowings, 23, 'Sarah Williams');
+console.log(eurowings);
