51e20d3d6dcb6d16b384e7966353c0f80fffed06
RiyazR2 <riyazpathan193.rp@gmail.com>
2023-11-16 13:05:18 +0530

    Bankist App Project Started

--- 11-Arrays-Bankist/script.js
+++ 11-Arrays-Bankist/script.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
@@ -6,28 +6,28 @@
 
 // Data
 const account1 = {
-  owner: "Jonas Schmedtmann",
+  owner: 'Jonas Schmedtmann',
   movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
   interestRate: 1.2, // %
   pin: 1111,
 };
 
 const account2 = {
-  owner: "Jessica Davis",
+  owner: 'Jessica Davis',
   movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
   interestRate: 1.5,
   pin: 2222,
 };
 
 const account3 = {
-  owner: "Steven Thomas Williams",
+  owner: 'Steven Thomas Williams',
   movements: [200, -200, 340, -300, -20, 50, 400, -460],
   interestRate: 0.7,
   pin: 3333,
 };
 
 const account4 = {
-  owner: "Sarah Smith",
+  owner: 'Sarah Smith',
   movements: [430, 1000, 700, 50, 90],
   interestRate: 1,
   pin: 4444,
@@ -36,44 +36,45 @@ const account4 = {
 const accounts = [account1, account2, account3, account4];
 
 // Elements
-const labelWelcome = document.querySelector(".welcome");
-const labelDate = document.querySelector(".date");
-const labelBalance = document.querySelector(".balance__value");
-const labelSumIn = document.querySelector(".summary__value--in");
-const labelSumOut = document.querySelector(".summary__value--out");
-const labelSumInterest = document.querySelector(".summary__value--interest");
-const labelTimer = document.querySelector(".timer");
+const labelWelcome = document.querySelector('.welcome');
+const labelDate = document.querySelector('.date');
+const labelBalance = document.querySelector('.balance__value');
+const labelSumIn = document.querySelector('.summary__value--in');
+const labelSumOut = document.querySelector('.summary__value--out');
+const labelSumInterest = document.querySelector('.summary__value--interest');
+const labelTimer = document.querySelector('.timer');
 
-const containerApp = document.querySelector(".app");
-const containerMovements = document.querySelector(".movements");
+const containerApp = document.querySelector('.app');
+const containerMovements = document.querySelector('.movements');
 
-const btnLogin = document.querySelector(".login__btn");
-const btnTransfer = document.querySelector(".form__btn--transfer");
-const btnLoan = document.querySelector(".form__btn--loan");
-const btnClose = document.querySelector(".form__btn--close");
-const btnSort = document.querySelector(".btn--sort");
+const btnLogin = document.querySelector('.login__btn');
+const btnTransfer = document.querySelector('.form__btn--transfer');
+const btnLoan = document.querySelector('.form__btn--loan');
+const btnClose = document.querySelector('.form__btn--close');
+const btnSort = document.querySelector('.btn--sort');
 
-const inputLoginUsername = document.querySelector(".login__input--user");
-const inputLoginPin = document.querySelector(".login__input--pin");
-const inputTransferTo = document.querySelector(".form__input--to");
-const inputTransferAmount = document.querySelector(".form__input--amount");
-const inputLoanAmount = document.querySelector(".form__input--loan-amount");
-const inputCloseUsername = document.querySelector(".form__input--user");
-const inputClosePin = document.querySelector(".form__input--pin");
+const inputLoginUsername = document.querySelector('.login__input--user');
+const inputLoginPin = document.querySelector('.login__input--pin');
+const inputTransferTo = document.querySelector('.form__input--to');
+const inputTransferAmount = document.querySelector('.form__input--amount');
+const inputLoanAmount = document.querySelector('.form__input--loan-amount');
+const inputCloseUsername = document.querySelector('.form__input--user');
+const inputClosePin = document.querySelector('.form__input--pin');
 
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
 
-// const currencies = new Map([
-//   ["USD", "United States dollar"],
-//   ["EUR", "Euro"],
-//   ["GBP", "Pound sterling"],
-// ]);
+const currencies = new Map([
+  ['USD', 'United States dollar'],
+  ['EUR', 'Euro'],
+  ['GBP', 'Pound sterling'],
+]);
 
-// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
-
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 /////////////////////////////////////////////////
+
+/****************************** PROJECT: "Bankist" App ******************************/
 
 /****************************** Simple Array Methods ******************************/
 /*
@@ -140,10 +141,10 @@ console.log("Riyaz".at(-2)); // a
 */
 /****************************** forEach Loop ******************************/
 // break statement and Continue statement is not work in ForEach Loop
-
+/*
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
-console.log("--------- for-of Loop -----------");
+console.log('--------- for-of Loop -----------');
 for (const movement of movements) {
   if (movement > 0) {
     console.log(`You Deposited ${movement}`);
@@ -152,7 +153,7 @@ for (const movement of movements) {
   }
 }
 
-console.log("--------- forEach Loop -----------");
+console.log('--------- forEach Loop -----------');
 movements.forEach(function (movement) {
   if (movement > 0) {
     console.log(`You Deposited ${movement}`);
@@ -161,7 +162,7 @@ movements.forEach(function (movement) {
   }
 });
 
-console.log("--------- for-of Loop with entries -----------");
+console.log('--------- for-of Loop with entries -----------');
 
 for (const [i, movement] of movements.entries()) {
   if (movement > 0) {
@@ -171,7 +172,7 @@ for (const [i, movement] of movements.entries()) {
   }
 }
 
-console.log("--------- forEach Loop with index and array -----------");
+console.log('--------- forEach Loop with index and array -----------');
 movements.forEach(function (mov, i, arr) {
   if (mov > 0) {
     console.log(`Movement ${i + 1}: You Deposited ${mov}`);
@@ -181,12 +182,14 @@ movements.forEach(function (mov, i, arr) {
 });
 
 console.log(`***********************************************************`);
-/****************************** forEach Loop with Maps and Sets ******************************/
+*/
 
+/****************************** forEach Loop with Maps and Sets ******************************/
+/*
 const currencies = new Map([
-  ["USD", "United States dollar"],
-  ["EUR", "Euro"],
-  ["GBP", "Pound sterling"],
+  ['USD', 'United States dollar'],
+  ['EUR', 'Euro'],
+  ['GBP', 'Pound sterling'],
 ]);
 
 currencies.forEach(function (value, key, map) {
@@ -194,9 +197,12 @@ currencies.forEach(function (value, key, map) {
 });
 
 //SET
-const currenciesUnique = new Set(["USD", "GBP", "USD", "EUR", "EUR"]);
+const currenciesUnique = new Set(['USD', 'GBP', 'USD', 'EUR', 'EUR']);
 console.log(currenciesUnique);
 
 currenciesUnique.forEach(function (value, key, map) {
   console.log(`${key}: ${value}`);
 });
+
+console.log(`***********************************************************`);
+*/
